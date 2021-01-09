1567ba737db9dad941310b2f11880cb622b8ad0f
Mirjalol Rakhmonov <manhattan701@gmail.com>
2021-01-09 05:32:52 +0100

    Controller is being updated

--- expense-tracker-api/src/main/java/com/mirjalolcode/expensetracker/rest/UserRestController.java
+++ expense-tracker-api/src/main/java/com/mirjalolcode/expensetracker/rest/UserRestController.java
@@ -1,23 +1,38 @@
 package com.mirjalolcode.expensetracker.rest;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mirjalolcode.expensetracker.model.User;
+import com.mirjalolcode.expensetracker.service.UserService;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
+	
+	@Autowired
+	UserService userService;
  
 	@PostMapping("/register")
-	public String registerUser(@RequestBody Map<String, Object> userMap) {
+	public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap) {
 		String firstName=(String) userMap.get("firstName");
 		String lastName=(String) userMap.get("lastName");
 		String email=(String) userMap.get("email");
 		String password=(String) userMap.get("password");
 		
-		return firstName + ", " + lastName + ", " + email + ", " + password;
+		User user=userService.registerUser(firstName, lastName, email, password);
+		
+		Map<String, String> map=new HashMap<>();
+		map.put("message", "registered successfully");
+		
+		return new ResponseEntity<>(map, HttpStatus.OK);
 	}
 }
