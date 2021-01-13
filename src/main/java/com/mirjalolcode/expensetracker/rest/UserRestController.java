202a364afced978cdcf639b6cc69cc7d18aa2013
Piotr Szymanski <piotr.szymanski@azimo.com>
2021-01-13 14:22:12 +0100

    directory structure simplified

--- src/main/java/com/mirjalolcode/expensetracker/rest/UserRestController.java
+++ src/main/java/com/mirjalolcode/expensetracker/rest/UserRestController.java
@@ -1,8 +1,7 @@
 package com.mirjalolcode.expensetracker.rest;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.mirjalolcode.expensetracker.model.User;
+import com.mirjalolcode.expensetracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mirjalolcode.expensetracker.model.User;
-import com.mirjalolcode.expensetracker.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
