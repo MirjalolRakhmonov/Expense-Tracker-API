d933b438f50f1149841664ec9fc53d78220dca98
Piotr Szymanski <piotr.szymanski@azimo.com>
2021-01-13 22:09:03 +0100

    test your code !!! see: spockframework

---
+++ src/test/java/com/mirjalolcode/expensetracker/service/UserServiceImplTest.groovy
@@ -0,0 +1,20 @@
+package com.mirjalolcode.expensetracker.service
+
+import com.mirjalolcode.expensetracker.exception.EtAuthException
+import com.mirjalolcode.expensetracker.repository.UserRepository
+import spock.lang.Specification
+
+class UserServiceImplTest extends Specification {
+
+    def repository = GroovyMock(UserRepository)
+
+    def "should throw exception when user provided not well formatted email"() {
+        given:
+            def sut = new UserServiceImpl(repository)
+        when:
+            sut.registerUser("firstName", "lastName", "wrongEmail", "pass")
+        then:
+            thrown(EtAuthException.class)
+    }
+
+}
